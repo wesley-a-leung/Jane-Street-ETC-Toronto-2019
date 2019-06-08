b6e84eb7bd6b48d46a2745cd6328f24a98476223
wesley-a-leung <wesley.a.leung@gmail.com>
2019-06-08 11:19:54 -0400

    Updated Bonds

--- PrivateFeed.cpp
+++ PrivateFeed.cpp
@@ -43,5 +43,19 @@ int main(int argc, char *argv[]) {
             return 0;
         }
     }
+
+
+    // std::vector<std::string> data;
+    // data.push_back(std::string("HELLO"));
+    // data.push_back(config.team_name);
+    
+    //   A common mistake people make is to conn.send_to_exchange() > 1
+    //   time for every conn.read_from_exchange() response.
+    //   Since many write messages generate marketdata, this will cause an
+    //   exponential explosion in pending messages. Please, don't do that!
+    
+    // conn.send_to_exchange(join(" ", data));
+    // std::string line = conn.read_from_exchange();
+    // std::cout << "The exchange replied: " << line << std::endl;
     return 0;
 }
