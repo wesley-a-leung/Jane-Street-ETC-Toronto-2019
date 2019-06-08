f694fed7e6f2b089bc615cdbc79db946f6cf24cc
wesley-a-leung <wesley.a.leung@gmail.com>
2019-06-08 11:24:39 -0400

    Crashing

--- PrivateFeed.cpp
+++ PrivateFeed.cpp
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
                 cout << line << endl;
             }
         } catch (runtime_error &e) {
+            assert(false, "Exchange Crashed");
             return 0;
         }
     }
