4954e9672ccf4892a86cc599412339460623632a
tangsaidi <tangsaidi@icloud.com>
2019-06-08 15:51:10 -0400

    d

--- Bonds.1.cpp
+++ Bonds.1.cpp
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
                     if (stockToSellID[tokens[1]]) {
                         conn.send_to_exchange(cancel(stockToSellID[tokens[1]]));
                     }
-                    conn.send_to_exchange(sellStock(currentId, (int) fairval * 1.001, 10, tokens[1]));
+                    conn.send_to_exchange(sellStock(currentId, (int) fairval * 1.02, 10, tokens[1]));
                     stockToSellID[tokens[1]] = currentId;
                     currentId++;
 
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
                     if (stockToBuyID[tokens[1]]) {
                         conn.send_to_exchange(cancel(stockToBuyID[tokens[1]]));
                     }
-                    conn.send_to_exchange(buyStock(currentId, (int) fairval * 0.999, 10, tokens[1]));
+                    conn.send_to_exchange(buyStock(currentId, (int) fairval * 0.98, 10, tokens[1]));
                     stockToBuyID[tokens[1]] = currentId;
                     currentId++;
                 }
