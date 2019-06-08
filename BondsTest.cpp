d3dd2b0ebfb2c5e1e56ebcf5b33ec884897d5189
wesley-a-leung <wesley.a.leung@gmail.com>
2019-06-08 17:21:41 -0400

    Bonds Test

--- BondsTest.cpp
+++ BondsTest.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 unordered_set<int> buys, sells;
 unordered_map<int, string> pending;
+unordered_map<string, int> buyPrice, sellPrice;
 
 int main(int argc, char *argv[]) {
     assert(argc <= 3);
@@ -18,6 +19,7 @@ int main(int argc, char *argv[]) {
     cout << conn.read_from_exchange() << endl;
 
     int currentId = 1;
+    pair<int, int> lastVAL = make_pair(-1, -1);
     queue<int> q;
     for (int cnt = 0; ; cnt++) {
         try {
@@ -27,38 +29,60 @@ int main(int argc, char *argv[]) {
                 q.pop();
             }
             vector<string> tokens = split(line, ' ');
-            if (!startsWith(tokens[0], {"BOOK", "TRADE"})) {
-                if (!startsWith(tokens[0], {"REJECT"})) cout << line << endl;
-                if (startsWith(tokens[0], {"FILL"})) {
+            if (tokens[0] != "BOOK" && tokens[0] != "TRADE") {
+                if (tokens[0] != "TRADE") cout << line << endl;
+                if (tokens[0] == "FILL") {
                     int id = stoi(tokens[1]);
                     int sz = stoi(tokens[5]);
-                    if (buys.count(id)) {
-                        pending[currentId] = buyBond(currentId, 998, sz);
-                        q.push(currentId);
-                        buys.insert(currentId++);
-                    } else {
-                        pending[currentId] = sellBond(currentId, 1002, sz);
-                        q.push(currentId);
-                        sells.insert(currentId++);
+                    string stock = tokens[2];
+                    if (stock == "BOND") {
+                        if (buys.count(id)) {
+                            pending[currentId] = buyBond(currentId, 999, sz);
+                            q.push(currentId);
+                            buys.insert(currentId++);
+                        } else {
+                            pending[currentId] = sellBond(currentId, 1001, sz);
+                            q.push(currentId);
+                            sells.insert(currentId++);
+                        }
                     }
-                } else if (startsWith(tokens[0], {"OUT"})) {
+                } else if (tokens[0] == "OUT") {
                     int id = stoi(tokens[1]);
                     buys.erase(id);
                     sells.erase(id);
-                } else if (startsWith(tokens[0], {"OPEN"})) {
-                    conn.send_to_exchange(buyBond(currentId, 998, 100));
+                    if (id == lastVAL.first) lastVAL.first = -1;
+                    if (id == lastVAL.second) lastVAL.second = -1;
+                } else if (tokens[0] == "OPEN") {
+                    conn.send_to_exchange(buyBond(currentId, 999, 100));
                     buys.insert(currentId++);
-                    conn.send_to_exchange(sellBond(currentId, 1002, 100));
+                    conn.send_to_exchange(sellBond(currentId, 1001, 100));
                     sells.insert(currentId++);
-                } else if (startsWith(tokens[0], {"CLOSE"})) {
-                    cout << "CLOSE" << endl;
+                } else if (tokens[0] == "CLOSE") {
                     return 0;
-                } else if (startsWith(tokens[0], {"ACK"})) {
+                } else if (tokens[0] == "FILL") {
                     int id = stoi(tokens[1]);
                     pending.erase(id);                    
-                } else if (startsWith(tokens[0], {"REJECT"})) {
+                } else if (tokens[0] == "REJECT") {
                     int id = stoi(tokens[1]);
                     q.push(id);
+                }
+            } else if (tokens[0] == "BOOK") {
+                pair<string, int> bp = getBuyPrice(line);
+                pair<string, int> sp = getSellPrice(line);
+                buyPrice[bp.first] = bp.second;
+                sellPrice[sp.first] = sp.second;
+            }
+            if (sellPrice["VALE"] < buyPrice["VALBZ"]) {
+                if (lastVAL == make_pair(-1, -1)) {
+                    pending[currentId] = convertStock(currentId, sellPrice["VALE"], 10, "VALE");
+                    lastVAL.first = currentId;
+                    q.push(currentId++);
+                    pending[currentId] = sellStock(currentId, buyPrice["VALBZ"], 10, "VALBZ");
+                    lastVAL.second = currentId;
+                    q.push(currentId++);
+                } else {
+                    cancel(lastVAL.first);
+                    cancel(lastVAL.second);
                 }
             }
         } catch (runtime_error &e) {
